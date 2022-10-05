18a5380c4ad11ea5ebc5821701f8fb4804246d9c
jjwonderh01_git <115084239+jjwonderh01@users.noreply.github.com>
2022-10-05 18:23:00 +0530

    Update streamlit_app.py
    change menu to test-type

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 streamlit.title('My Parents new healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and Blueberry Meal')
-streamlit.header('Kale, Spinach and Rocket smoothie')
-streamlit.header('Hard-Boiled free range egg')
+streamlit.text('Kale, Spinach and Rocket smoothie')
+streamlit.text('Hard-Boiled free range egg')
