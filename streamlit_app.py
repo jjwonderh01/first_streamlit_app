c3496c707fc4bdd9cc2b94fdfbe599908596b5bb
jjwonderh01_git <115084239+jjwonderh01@users.noreply.github.com>
2022-10-07 10:17:15 +0530

    Update streamlit_app.py
    tabular format

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,10 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+
+# normalise the data 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+# convert to dataframe for tabular format
+streamlit.dataframe(fruityvice_normalized)
+
