a44462f77472b5132556bd06a7433a5ad9e56185
jjwonderh01_git <115084239+jjwonderh01@users.noreply.github.com>
2022-10-07 10:18:03 +0530

    Update streamlit_app.py
    remove JSON output

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.header('Fruityvice fruit Advice!')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
 
 # normalise the data 
